d7610f46f4f19ad576ffd6c991f16d30cb435f7a
Fjred <karolis.aukstakojis@gmail.com>
2023-04-02 17:02:04 +0300

    Walk n Jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
-    public Rigidbody rb;
+    public Rigidbody2D rb;
+    public float jumpSpeed;
+
     void Start()
     {
         
@@ -14,21 +16,28 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        
+        Walk();
+        
+
         Jump();
-
-        rb.velocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.D))
-            rb.velocity = new Vector3(speed * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.A))
-            rb.velocity = new Vector3(-speed * Time.deltaTime, 0, 0);
-
     }
 
+    void Walk()
+    {
+        
+
+        if (Input.GetKey(KeyCode.D))
+            rb.velocity = new Vector2(speed, 0);
+        if (Input.GetKey(KeyCode.A))
+            rb.velocity = new Vector2(-speed, 0);
+    }
     void Jump()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //rb.velocity = new Vector2(0, jumpSpeed);
+            rb.velocity = new Vector2(0, jumpSpeed);
         }
     }
 }
