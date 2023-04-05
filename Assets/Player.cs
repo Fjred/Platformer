1a05080cc4b48322f1125da84d3067f5e3df99d4
Fjred <karolis.aukstakojis@gmail.com>
2023-04-05 17:15:10 +0300

    walk and jump
    Some platforms also

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,40 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    public Rigidbody2D rb;
-    public float jumpSpeed;
+    public float moveSpeed;
 
+    public float jumpSpeed = 10;
+
+    public float jumpHeight = 3;
+
+    private Rigidbody2D rb;
+
+
+    public bool isOnGround = false;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     private void Update()
     {
         
-        Walk();
-        
 
-        Jump();
-    }
 
-    void Walk()
-    {
-        
 
-        if (Input.GetKey(KeyCode.D))
-            rb.velocity = new Vector2(speed, 0);
-        if (Input.GetKey(KeyCode.A))
-            rb.velocity = new Vector2(-speed, 0);
-    }
-    void Jump()
-    {
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) & isOnGround)
         {
-            rb.velocity = new Vector2(0, jumpSpeed);
+            var speed = Mathf.Sqrt(jumpHeight * -rb.gravityScale * Physics2D.gravity.y * 2);
+            rb.velocity = Vector2.up * speed;
+
+            isOnGround = false;
         }
+
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+
+        
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOnGround = false;
     }
 }
