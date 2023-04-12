24347a7b8b3acb2cef481d4881d2fe8b2a173413
Fjred <karolis.aukstakojis@gmail.com>
2023-04-12 16:42:03 +0300

    Hover & coin

---
+++ Assets/Hover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public float height = 0.1f;
+    public float speed = 10f;
+
+    private float offset;
+
+    private void Start()
+    {
+        offset = Random.Range(0f, 100f);
+    }
+    void Update()
+    {
+        transform.localPosition = Vector3.up * Mathf.Sin(offset * Time.time * speed * 6.28f) * height;
+    }
+}
